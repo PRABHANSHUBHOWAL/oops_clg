7a6754bbb1173e5915d616850c8616663757d978
PRABHANSHUBHOWAL <prabhanshubhowal@gmail.com>
2023-11-24 21:49:30 +0530

    .\Rectangle.java added

---
+++ OOP_lab/Rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle extends Figure{
+	
+	Rectangle(int length, int breadth){
+		super(length, breadth);
+	}
+	
+	public int area(){
+		return super.var1 * super.var2;
+	}
+	
+	
+	
+}
+
+
