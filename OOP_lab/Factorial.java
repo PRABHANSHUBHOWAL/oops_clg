46271e9de540012a78e114c92f01703936068386
PRABHANSHUBHOWAL <prabhanshubhowal@gmail.com>
2023-11-27 12:22:30 +0530

    .\Factorial.java added

--- OOP_lab/Factorial.java
+++ OOP_lab/Factorial.java
@@ -1,20 +1,20 @@
 import java.util.*;
+
 class Factorial {
-	public static void main(String  args[]) {
+	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter no. ");
 		int n = sc.nextInt();
-		if(n < 0) {
+		if (n < 0) {
 			System.out.println("Can't find factorial");
-			}
-		if(n == 0) {
+		}
+		if (n == 0) {
 			System.out.println(1);
-			}
-			
-		int fact=1;
-		for(int i = 2; i <=n; i++) {
-		fact *= i;
 		}
-		System.out.println("Factorial is "+ fact);
+		int fact = 1;
+		for (int i = 2; i <= n; i++) {
+			fact *= i;
 		}
-		}
+		System.out.println("Factorial is " + fact);
+	}
+}
